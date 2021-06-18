beeb486d79705a8b1b5800a627082d42c4ab4328
pradeep.j@zohocorp.com <pradeep.j@zohocorp.com>
2021-06-19 00:53:51 +0530

    deploying to heroku

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const app = new express();
 
-const port = 3000;
+const PORT = process.env.PORT || '3300';
 
 app.get("/",(req,res)=>{
 	res.send("hello world");
 })
 
-app.listen(port,()=>{
-	console.log(`Simple Tweeter is online and listening on http://localhost:${port}`);
+app.listen(PORT,(err)=>{
+
+	err ? console.error(err) : console.log(`Simple Tweeter is online and listening on http://localhost:${PORT}`);
 })

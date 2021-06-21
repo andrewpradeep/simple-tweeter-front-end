6ba6be8d16b3d778489f775c1e4f05a8c824eca9
pradeep.j@zohocorp.com <pradeep.j@zohocorp.com>
2021-06-22 02:15:09 +0530

    added webpack cli for build configutation and removed it from node js code, added package-lock.json to .gitignore

--- index.js
+++ index.js
@@ -5,28 +5,6 @@ const path = require("path");
 
 console.log("initiazing application");
 
-function runWebPack()
-{
-	const config = require(path.resolve(__dirname,"./webapps/webpack.prod"));
-	const webpack = require("webpack");
-	const compiler = webpack(config);
-
-	return  new Promise((resolve,reject)=>{
-		compiler.run((err,res)=>{
-			if(err)
-			{
-				reject(err);
-			}
-			else
-			{
-				console.log("webpack successfull");
-				resolve(res)
-			}
-		})
-	});
-}
-
-
 app.get("/",(req,res)=>{
 	res.sendFile(path.resolve(__dirname,"./webapps/dist/index.html"))
 })
@@ -34,10 +12,8 @@ app.get("/",(req,res)=>{
 app.get("/webapps/dist/:filename",(req,res)=>{
 	res.sendFile(path.resolve(__dirname,`./webapps/dist/${req.params.filename}`));
 })
-console.log("starting webpack ....");
-runWebPack().then(()=>{
-	app.listen(PORT,(err)=>{
-		err ? console.error(err) : console.log(`Simple Tweeter is online and listening on http://localhost:${PORT}`);
-	})
-})
+
+app.listen(PORT,(err)=>{
+	err ? console.error(err) : console.log(`Simple Tweeter is online and listening on http://localhost:${PORT}`);
+});
 
